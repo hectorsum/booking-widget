c2ebf8998e65b146fd7b712e343b32ef5b9a5958
Hector Herrera <hectorsum81@hotmail.com>
2021-09-09 09:10:53 -0500

    date updates added

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const setStartDate = moment(new Date()).format("ddd, MMM Do");
-const setEndDate = moment(new Date()).add(1, 'days').format("ddd, MMM Do");
-document.getElementById('check-in-out').innerText = `${setStartDate} - ${setEndDate}`;
+const url = new URL(window.location.href);
+const aDate = url.searchParams.get("aDate");
+const dDate = url.searchParams.get("dDate");
+const checkin_hidden = document.getElementById('checkin-hidden');
+const checkout_hidden = document.getElementById('checkout-hidden');
+
+if(!aDate && !dDate){
+  const setStartDate = moment(new Date()).format("ddd, MMM Do");
+  const setEndDate = moment(new Date()).format("ddd, MMM Do");
+  document.getElementById('check-in-out').innerText = `${setStartDate} - ${setEndDate}`;
+  checkin_hidden.value = moment(new Date()).format("YYYY-MM-DD");
+  checkout_hidden.value = moment(new Date()).format("YYYY-MM-DD");
+}else{
+  const setStartDate = moment(aDate).format("ddd, MMM Do");
+  const setEndDate = moment(dDate).format("ddd, MMM Do");
+  document.getElementById('check-in-out').innerText = `${setStartDate} - ${setEndDate}`;
+  checkin_hidden.value = aDate;
+  checkout_hidden.value = dDate;
+}
 
 const picker = new Litepicker({
   element: document.getElementById('check-in-out'),
@@ -22,17 +38,14 @@ const picker = new Litepicker({
       const {
         dateInstance: dateend
       } = picker.getEndDate()
-      // popup1.style.display = "none"; //closing popup
       const setStartDate = moment(datestart).format("ddd, MMM Do");
       const setEndDate = moment(dateend).format("ddd, MMM Do");
       console.log({setStartDate,setEndDate});
-      const checkin_hidden = document.getElementById('checkin-hidden');
-      const checkout_hidden = document.getElementById('checkout-hidden');
-      checkin_hidden.value = moment(datestart).format("D-MMM-YY");
-      checkout_hidden.value = moment(dateend).format("D-MMM-YY");;
+      
+      checkin_hidden.value = moment(datestart).format("YYYY-MM-DD");
+      checkout_hidden.value = moment(dateend).format("YYYY-MM-DD");
 
       document.getElementById('check-in-out').innerText = `${setStartDate} - ${setEndDate}`;
-      // document.getElementById('check-out-date-HA').innerHTML = setEndDate;
     });
   },
 });
